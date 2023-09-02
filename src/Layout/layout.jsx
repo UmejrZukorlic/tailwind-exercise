ad568392f1debced15e0d29c110eb9830354ed88
Umejr Zukorlic <89653651+UmejrZukorlic@users.noreply.github.com>
2023-09-02 12:11:34 +0200

    feat: finish styling nav bar

--- src/Layout/layout.jsx
+++ src/Layout/layout.jsx
@@ -1,71 +1,91 @@
 import React, { useState } from "react";
 import photo from "../assets/logoTest.png";
-import {
-  Menu,
-  MenuHandler,
-  MenuList,
-  MenuItem,
-  Button,
-} from "@material-tailwind/react";
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 
 const Layout = () => {
   const [arrow, setArrow] = useState(false);
+  const [arrow2, setArrow2] = useState(false);
+
   console.log(arrow);
   return (
     <div>
-      <nav className="fixed top-0 w-full flex justify-between items-center px-10 bg-green-800">
-        <img src={photo} alt="slika" className="max-h-24 " />
-        <ul className="w-2/3 list-none  flex justify-evenly items-center  text-white font-mono">
-          <li className="w-22">POCETNA</li>
-          <li className="w-22">O NAMA</li>
-          <li className="w-22">
-            <Menu
-              placement="bottom-start"
-              onClick={() => {
-                console.log(arrow);
-                setArrow(!arrow);
-              }}>
-              <MenuHandler
-                className="bg-transparent shadow-none flex items-center hover:shadow-none active:opacity-100 focus:opacity-100"
-                onChange={() => {
-                  console.log(arrow);
-                  setArrow(!arrow);
-                }}>
-                <Button className="bg-transparent shadow-none flex items-center">
-                  <p className="text-base font-medium">Stanovi U Ponudi</p>
-                  {!arrow ? (
-                    <MdKeyboardArrowDown className="text-xl" />
-                  ) : (
-                    <MdKeyboardArrowUp className="text-xl" />
-                  )}
-                </Button>
-              </MenuHandler>
-              <MenuList>
-                <MenuItem>Garsonjere i Jednosobni Stanovi</MenuItem>
-                <MenuItem>Jednosobni Stanovi</MenuItem>
-                <MenuItem>Dvosobni Stanovi</MenuItem>
-                <MenuItem>Dvoiposobni Stanovi</MenuItem>
-                <MenuItem>Trosobni Stanovi</MenuItem>
-              </MenuList>
-            </Menu>
+      <nav className="fixed top-0 w-full flex justify-between items-center px-10 py-1 bg-blue-gray-900">
+        <img src={photo} alt="slika" className="max-h-24 mx-20" />
+        <ul className="w-2/3 list-none  flex justify-center items-center  text-white font-mono">
+          <li className="px-10 py-1 hover:bg-blue-gray-800 transition-all">
+            POCETNA
           </li>
-          <li className="w-22">
-            <Menu placement="bottom-start">
-              <MenuHandler className=" w-full bg-transparent shadow-none  hover:shadow-none active:opacity-100 focus:opacity-100">
-                <Button className=" w-full bg-transparent shadow-none ">
-                  <p className="w-full font-sans text-base font-medium ">
-                    ULAZI
-                  </p>
-                </Button>
-              </MenuHandler>
-              <MenuList>
-                <MenuItem>ULAZ A</MenuItem>
-                <MenuItem>ULAZ B</MenuItem>
-              </MenuList>
-            </Menu>
+          <li className="border-s-2 border-e-2 px-10 py-1 hover:bg-blue-gray-800 transition-all">
+            O NAMA
           </li>
-          <li className="w-22">KONTAKT</li>
+          <li
+            className="px-10 py-1 hover:bg-blue-gray-800 transition-all"
+            onClick={() => {
+              setArrow(!arrow);
+              setArrow2(false);
+            }}>
+            <div className="flex items-center active:bg-blue-gray-90  transition-all">
+              <p>STANOVI U PONUDI</p>
+              {!arrow ? (
+                <MdKeyboardArrowDown className="text-xl" />
+              ) : (
+                <MdKeyboardArrowUp className="text-xl" />
+              )}
+            </div>
+
+            {arrow && (
+              <div className="bg-blue-gray-900 absolute mt-5 p-2">
+                <ul>
+                  <li> </li>
+                  <li className="py-1 px-3 hover:bg-blue-gray-800 transition-all">
+                    Garsonjere i Jednosobni Stanovi
+                  </li>
+                  <li className="py-1 px-3 hover:bg-blue-gray-800 transition-all">
+                    Jednosobni Stanovi
+                  </li>
+                  <li className="py-1 px-3 hover:bg-blue-gray-800 transition-all">
+                    Dvosobni Stanovi
+                  </li>
+                  <li className="py-1 px-3 hover:bg-blue-gray-800 transition-all">
+                    Dvoiposobni Stanovi
+                  </li>
+                  <li className="py-1 px-3 hover:bg-blue-gray-800 transition-all">
+                    Trosobni Stanovi
+                  </li>
+                </ul>
+              </div>
+            )}
+          </li>
+          <li
+            className="px-10 py-1 border-s-2 border-e-2 hover:bg-blue-gray-800 transition-all"
+            onClick={() => {
+              setArrow2(!arrow2);
+              setArrow(false);
+            }}>
+            <div className=" flex items-center active:bg-blue-gray-800 transition-all">
+              <p>ULAZI</p>
+              {!arrow2 ? (
+                <MdKeyboardArrowDown className="text-xl" />
+              ) : (
+                <MdKeyboardArrowUp className="text-xl" />
+              )}
+            </div>
+            {arrow2 && (
+              <div className="bg-blue-gray-900 absolute mt-5 p-2 ">
+                <ul>
+                  <li className="w-40 py-1 px-3 hover:bg-blue-gray-800 transition-all">
+                    ULAZ A{" "}
+                  </li>
+                  <li className="w-40 py-1 px-3 hover:bg-blue-gray-800 transition-all">
+                    ULAZ B{" "}
+                  </li>
+                </ul>
+              </div>
+            )}
+          </li>
+          <li className="px-10 py-1 hover:bg-blue-gray-800 transition-all">
+            KONTAKT
+          </li>
         </ul>
       </nav>
     </div>

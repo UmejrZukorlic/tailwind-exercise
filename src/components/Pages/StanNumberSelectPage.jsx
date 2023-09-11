9f4efb744bb11dcb1b01191d186c7607bc178a82
Umejr Zukorlic <89653651+UmejrZukorlic@users.noreply.github.com>
2023-09-11 16:41:13 +0200

    feat: data.json update

--- src/components/Pages/StanNumberSelectPage.jsx
+++ src/components/Pages/StanNumberSelectPage.jsx
@@ -47,7 +47,7 @@ const StanNumberSelectPage = () => {
       <div className="relative z-10 bg-teal-50 flex flex-col items-center">
         <div className="w-full flex justify-center items-center bg-teal-300 py-12">
           <h1 className="text-white text-4xl bg-t">
-            {ulaz && "ULAZ " + ulaz} - {stanType?.toUpperCase()}{" "}
+            {ulaz && "ULAZ " + ulaz + " - "} {stanType?.toUpperCase()}{" "}
           </h1>
         </div>
         <div className="w-[80%] py-8 grid gap-8 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 place-items-center">
@@ -55,16 +55,15 @@ const StanNumberSelectPage = () => {
             return (
               <div
                 key={i}
-                className="border-2 border-solid border-teal-600 bg-white py-4">
-                <h1 className="text-center text-xl ">
-                  {el.name.toUpperCase()}
-                </h1>
-                <p className="text-center text-xl pb-4">
+                className="border-2 border-solid border-teal-600 bg-white py-4 text-xl">
+                <h1 className="text-center">{el.name.toUpperCase()}</h1>
+                <p className="text-center pb-4">
                   POVRŠINA STANA:{" "}
                   <span className="font-bold">{el.površinaStana}</span> m
                   <sup>2</sup>
                 </p>
                 <img src={el.imgUrl} alt="slika" />
+                {!lamela && <p className="text-center mt-4">ULAZ {el.ulaz}</p>}
               </div>
             );
           })}
